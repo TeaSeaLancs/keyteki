0bf80c2de3a9272cbe7358de6b4d980f07909409
Matt Harker <matt@theflux.co.uk>
2019-11-21 16:49:24 +0000

    Add a replicable test case for aember capture happening on attacking creatures that are destroyed

--- test/server/cards/03-WC/ShatteredThrone.spec.js
+++ test/server/cards/03-WC/ShatteredThrone.spec.js
@@ -5,32 +5,34 @@ describe('Shattered Throne', function() {
                 this.setupTest({
                     player1: {
                         amber: 3,
-                        house: 'brobnar',
-                        inPlay: ['brammo', 'ganger-chieftain', 'foozle', 'culf-the-quiet', 'shattered-throne']
+                        house: 'saurian',
+                        hand: ['praefectus-ludo'],
+                        inPlay: ['crassosaurus', 'odoac-the-patrician']
                     },
                     player2: {
                         amber: 3,
-                        inPlay: ['silvertooth', 'gamgee', 'krump', 'troll']
+                        inPlay: ['silvertooth', 'gamgee', 'krump', 'troll', 'shattered-throne']
                     }
                 });
             });
 
             it('should capture an amber after fight and survive', function() {
-                this.player1.fightWith(this.brammo, this.silvertooth);
-                expect(this.brammo.amber).toBe(1);
+                this.player1.fightWith(this.crassosaurus, this.silvertooth);
+                expect(this.crassosaurus.amber).toBe(1);
                 expect(this.silvertooth.amber).toBe(0);
                 expect(this.player2.amber).toBe(2);
             });
 
-            it('should capture an amber after fight and return it if it does not survive', function() {
-                this.player1.fightWith(this.foozle, this.troll);
-                expect(this.foozle.hasToken('amber')).toBe(false);
+            it('should not capture amber after fight if it does not survive', function() {
+                this.player1.play(this.praefectusLudo);
+                this.player1.fightWith(this.odoacThePatrician, this.troll);
+                expect(this.odoacThePatrician.hasToken('amber')).toBe(false);
                 expect(this.player2.amber).toBe(3);
             });
 
             it('should capture an amber after fighting an elusive', function() {
-                this.player1.fightWith(this.brammo, this.gamgee);
-                expect(this.brammo.amber).toBe(1);
+                this.player1.fightWith(this.crassosaurus, this.gamgee);
+                expect(this.crassosaurus.amber).toBe(1);
                 expect(this.gamgee.amber).toBe(0);
                 expect(this.player2.amber).toBe(2);
             });
@@ -38,9 +40,9 @@ describe('Shattered Throne', function() {
             it('should also be applicable for opponent', function() {
                 this.player1.endTurn();
                 this.player2.clickPrompt('shadows');
-                this.player2.fightWith(this.gamgee, this.culfTheQuiet);
+                this.player2.fightWith(this.gamgee, this.crassosaurus);
                 expect(this.gamgee.amber).toBe(1);
-                expect(this.culfTheQuiet.amber).toBe(0);
+                expect(this.crassosaurus.amber).toBe(0);
                 expect(this.player1.amber).toBe(2);
             });
         });
